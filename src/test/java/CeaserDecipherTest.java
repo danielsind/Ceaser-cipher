ae56f1bc558e4baf456043798b680c38a926ecb2
danielsind <danielsindet@gmail.com>
2020-01-28 16:07:13 +0300

    Created a test that checks for the instance of a String and an integer on CeaserDecipherTest.java

--- src/test/java/CeaserDecipherTest.java
+++ src/test/java/CeaserDecipherTest.java
@@ -1,2 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class CeaserDecipherTest {
+    @Test
+    public void canCreateAnInstanceOF_StringAndINteger()throws  Exception{
+        CeaserDecipher testDecipher = new CeaserDecipher("A",1);
+        assertEquals(true,testDecipher instanceof CeaserDecipher );
+    }
 }
