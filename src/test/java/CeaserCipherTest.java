1d5bb7b65b0ccab5bff2696c8ed7e6879364eb32
danielsind <danielsindet@gmail.com>
2020-01-28 12:38:59 +0300

    Added a new test that checks the instance of a string which came out as failed

--- src/test/java/CeaserCipherTest.java
+++ src/test/java/CeaserCipherTest.java
@@ -1,6 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-class CeaserCipherTest {
+import static org.junit.Assert.assertEquals;
 
-
+public class CeaserCipherTest {
+    // check if the class can be an instance of an empty array
+        @Test
+        public void  canCreate_InstatiatesCorrectly() throws Exception{
+            CeaserCipher testCeaserCipher = new CeaserCipher();
+            assertEquals(true,testCeaserCipher instanceof CeaserCipher);
+        }
+        //check if it can be an instance of a string;
+        @Test
+    public void newCipher_InstanceOfString() throws Exception{
+            CeaserCipher testCeaserCipher = new CeaserCipher("A");
+        }
 }
