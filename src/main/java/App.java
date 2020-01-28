6698abc07787481014d557d4338bdb5f26467035
danielsind <danielsindet@gmail.com>
2020-01-28 15:22:41 +0300

    Added to the file App.java A Welcome Statement

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class App {
+    public static void main (String[]args){
+        BufferedReader userWordReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("********************************");
+        System.out.println("Welcome to CAESER CIPHER");
+        System.out.println("*********************************");
+        System.out.println("Enter Your Word to get the Caeser Cipher for that word");
+    }
+    try{
+        String userWord = userWordReader.readLine();
+    }
 
 }
