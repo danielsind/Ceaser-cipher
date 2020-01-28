f9c8c0be83696816b6f7f72d0dd9b1ffc71f4101
danielsind <danielsindet@gmail.com>
2020-01-28 15:56:29 +0300

    Added on the App.java features that allow a user to enter a word and returns the ciphered word

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,28 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 public class App {
-    public static void main (String[]args){
+    public static void main (String[]args) {
         BufferedReader userWordReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader userKeyReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("********************************");
         System.out.println("Welcome to CAESER CIPHER");
         System.out.println("*********************************");
         System.out.println("Enter Your Word to get the Caeser Cipher for that word");
-    }
-    try{
-        String userWord = userWordReader.readLine();
+        try {
+            String userWord = userWordReader.readLine();
+            System.out.println("Enter the number of Shifts you would like to be made");
+            String  userKey = userKeyReader.readLine();
+            int intUserKey = Integer.parseInt(userKey);
+            CeaserCipher ceaserCipher = new CeaserCipher(userWord,intUserKey);
+            String ceaserOutPut = ceaserCipher.cipheredWord();
+            System.out.println("Your Ceaser Cypher is " + ceaserOutPut);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
