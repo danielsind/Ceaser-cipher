5166751e09fad08933e8679905ecfc1499490405
danielsind <danielsindet@gmail.com>
2020-01-29 12:06:34 +0300

    Created content on the App.java that allows a user to enter values and Submit to get a CIpher and Decipher strings

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,11 +5,12 @@ public class App {
     public static void main (String[]args) {
         BufferedReader userWordReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader userKeyReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader userChoice = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("********************************");
         System.out.println("Welcome to CAESER CIPHER");
         System.out.println("*********************************");
-        System.out.println("Enter Your Word to get the Caeser Cipher for that word");
         try {
+            System.out.println("Enter your Word");
             String userWord = userWordReader.readLine();
             System.out.println("Enter the number of Shifts you would like to be made");
             String  userKey = userKeyReader.readLine();
@@ -17,6 +18,15 @@ public class App {
             CeaserCipher ceaserCipher = new CeaserCipher(userWord,intUserKey);
             String ceaserOutPut = ceaserCipher.cipheredWord();
             System.out.println("Your Ceaser Cypher is " + ceaserOutPut);
+            System.out.println("Enter your Word to Decipher");
+            String userdWord = userWordReader.readLine();
+            System.out.println("Enter the number of Shifts you would like to be made");
+            String  userdKey = userKeyReader.readLine();
+            int intUserdKey = Integer.parseInt(userdKey);
+            CeaserDecipher ceaserDecipher = new CeaserDecipher(userdWord,intUserdKey);
+            String ceaserdOutPut = ceaserDecipher.decipheredWord();
+            System.out.println("Your Ceaser Decipher is " + ceaserdOutPut);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
